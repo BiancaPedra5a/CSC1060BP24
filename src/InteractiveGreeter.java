933aee99636fb20b63e224c672425c6fdb9d9222
BiancaPedr5a <bianca.pedraza0604@gmail.com>
2024-02-05 17:25:29 -0700

    InteractiveGreeter

---
+++ src/InteractiveGreeter.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class InteractiveGreeter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("Hello ! What is your name? ");
+        String name = input.next();
+        System.out.println("How old are you?");
+        int age = input.nextInt();
+        System.out.println("Where are you from?");
+        String location = input.next();
+        System.out
+    }
+}
