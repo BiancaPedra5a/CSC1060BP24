cb6ba751de2f091b02fc85c6401daa4c7fa7b599
BiancaPedr5a <bianca.pedraza0604@gmail.com>
2024-02-05 19:27:56 -0700

    Interactive Greeter

--- src/InteractiveGreeter.java
+++ src/InteractiveGreeter.java
@@ -9,6 +9,14 @@ public class InteractiveGreeter {
         int age = input.nextInt();
         System.out.println("Where are you from?");
         String location = input.next();
-        System.out
+        System.out.println( "Hi " + name + "! from " + location);
+        System.out.println("You are "+ age + " years old.");
+        System.out.println("What is your favorite thing to do?");
+        input.nextLine();
+        String favorite = input.nextLine();
+        System.out.println("Have fun next time you " + favorite +"!");
+        System.out.println("How much would you like to donate today? ");
+        double donation = input.nextDouble();
+        System.out.println("You will donate " + donation + " today! Thank you very much! ");
     }
 }
